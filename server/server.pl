20930084a72005fae8c23ef2f4936b0203fb071c
Peder Stray <peder.stray@gmail.com>
2015-08-11 07:24:33 +0200

    Added testing of global events

--- server/server.pl
+++ server/server.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
+
 use Mojolicious::Lite;
+use Mojo::IOLoop;
 
 my $id = 0;
 
@@ -12,11 +14,12 @@ get '/events' => sub {
     my $c = shift;
 
     # last id known to client
-    my $last_id = $c->req->headers->header('Last-Event-ID') || 0;
+    my $last_id = $c->req->headers->header('Last-Event-ID');
     
     # Increase the timeout, this is a long-"poll" after all...
     $c->inactivity_timeout(300);
 
+    Mojo::IOLoop->singleton->emit(eventer => 'started', sht21 => [ time, 3, 4 ]);
     
     $c->res->headers->content_type("text/event-stream");
     $c->res->headers->cache_control("no-cache");
@@ -25,20 +28,29 @@ get '/events' => sub {
     my $cb = $c->app->log->on(message => sub {
 				  my($log, $level, @lines) = @_;
 				  my $data = "[$level] @lines";
+				  my $id = time;
 				  $data =~ s/^/data: /mg;
 				  $c->write("id: $id\nevent: log\n$data\n\n");
-				  $id++;
 			      });
     $c->on(finish => sub {
 	       $c->app->log->unsubscribe(message => $cb);
 	   });
 
-    while ($id < $last_id) {
-	# fast forward to!
-	$id++;
+    if (defined $last_id) {
+	# push all items since last id
+	for ($last_id..time) {
+	    $c->write("event: log\n54\n\n");
+	}
     }
     
 } => 'events';
+
+Mojo::IOLoop->singleton->on(eventer => sub {
+					 my($e, @foo) = @_;
+					 
+					 printf "EVE: [%s] {%s}\n", $e, join "|", @foo;
+					 
+				     });
 
 app->secrets(['Some random secret string that nobody would guess']);
 app->start;
